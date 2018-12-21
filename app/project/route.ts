b1b1b525a0feab8d7b9a01677d5c7d0393485097
Ilya Radchenko <ilya@burstcreations.com>
2018-12-21 17:25:56 -0500

    fix(types): add project params type

--- app/project/route.ts
+++ app/project/route.ts
@@ -1,9 +1,12 @@
 import Route from '@ember/routing/route';
 
+export interface ProjectParams {
+  projectId: string;
+}
 export default class Project extends Route.extend({
   // anything which *must* be merged to prototype here
 }) {
-  model({ projectId }) {
+  model({ projectId }: ProjectParams) {
     return this.store.findRecord('project', projectId);
   }
 }
